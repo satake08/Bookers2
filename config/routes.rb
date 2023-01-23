03e10e70d032c2799c56c37423ef3cfac1d270f3
satake08 <117998354+satake08@users.noreply.github.com>
2023-01-23 15:57:50 +0000

    [add]5-27

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'books/new'
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
   devise_for :users
   get 'homes/top' =>"homes#top"
   get "homes/about" =>"homes#about", as: 'about'
